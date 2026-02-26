39ee686676a83941aee7fc7811a45a948d72af64
xMerPeople29 <noah.vanleeuwen@ontariotechu.net>
2026-02-26 13:28:29 -0500

    Reworked attribute storage for User and Car - Noah

--- src/Car.java
+++ src/Car.java
@@ -1,29 +1,57 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Car {
     //TODO: rework attribute vector to be HashMap<String,Double>, where each string is an attribute and each double is its value
-    private ArrayList<Double> carAttributes;
+    private HashMap<String, Double> carAttributes;
     private String make;
     private String model;
+    private String bodyType;
 
     public Car() {
-        this.carAttributes = new ArrayList<Double>();
+        this.carAttributes = new HashMap<>();
         this.make = null;
         this.model = null;
+        this.bodyType = null;
     }
 
-    public Car(ArrayList<Double> carAttributes, String make, String model) {
+    public Car(HashMap<String, Double> carAttributes, String make, String model, String bodyType) {
         this.carAttributes = carAttributes;
         this.make = make;
         this.model = model;
+        this.bodyType = bodyType;
     }
 
-    public ArrayList<Double> getCarAttributes() {
+    public HashMap<String, Double> getCarAttributes() {
         return carAttributes;
     }
 
     public String getMake() {
         return make;
+    }
+
+    public String getModel() {
+       return model;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public void setAttribute(String name, Double value) {
+        carAttributes.putIfAbsent(name, value);
     }
 
     public String toString() {
--- src/User.java
+++ src/User.java
@@ -1,64 +1,51 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class User {
-    public static final double ATTRIBUTE_MAX = 5;
     //TODO: rework attribute vector to be HashMap<String,Double>, where each string is an attribute and each double is its value
-    private static ArrayList<Double> userAttributes;
+    private static UserAttributeMap userAttributes;
     private static ArrayList<String> preferredMakes;
     private static ArrayList<String> preferredBodyTypes;
 
-    public User(ArrayList<Double> userAttributes, ArrayList<String> preferredMakes) {
+    public User(UserAttributeMap userAttributes, ArrayList<String> preferredMakes,
+                ArrayList<String> preferredBodyTypes) {
         this.userAttributes = userAttributes;
         this.preferredMakes = preferredMakes;
+        this.preferredBodyTypes = preferredBodyTypes;
+    }
+
+    public User() {
+        userAttributes = new UserAttributeMap();
+        preferredMakes = new ArrayList<>();
+        preferredBodyTypes = new ArrayList<>();
+    }
+
+    public void addAttribute(String name, Double value, Double importance) {
+        userAttributes.addAttribute(name, value, importance);
+    }
+
+    public void addPreferredMake(String make) {
+        preferredMakes.add(make);
+    }
+
+    public void addPreferredBodyType(String bodyType) {
+        preferredBodyTypes.add(bodyType);
     }
 
     public static double match(Car car) {
-        ArrayList<Double> carAttributes = car.getCarAttributes();
+        HashMap<String, Double> carAttributes = car.getCarAttributes();
         double distance = 0;
         if (!preferredMakes.contains(car.getMake())){
-            distance += Math.pow(ATTRIBUTE_MAX, 2);
+            distance += Math.pow(UserAttributeMap.ATTRIBUTE_MAX, 2);
         }
-        for(int i = 0; i < userAttributes.size(); i++) {
-            distance += Math.pow( (userAttributes.get(i) - carAttributes.get(i)) , 2);
-
+        if (!preferredBodyTypes.contains(car.getBodyType())) {
+            distance += Math.pow(UserAttributeMap.ATTRIBUTE_MAX, 2);
+        }
+        for(String attribute : userAttributes.getAttributeNames()) {
+            distance += Math.pow( (userAttributes.getValue(attribute) - carAttributes.get(attribute)) , 2)
+                    * userAttributes.getImportance(attribute);
         }
         return Math.sqrt(distance);
     }
 
-    //TODO: create a new class to actually run the program, this main function is just for testing purposes
-    public static void main(String[] args) {
-        ArrayList<Double> attributes1 = new ArrayList<>(Arrays.asList(1.0,2.4,3.2));
-        ArrayList<Double> attributes2 = new ArrayList<>(Arrays.asList(2.0,0.4,2.3));
-        ArrayList<Double> attributes3 = new ArrayList<>(Arrays.asList(0.2,0.2,0.9));
-        Car car1 = new Car(attributes1, "Toyota", "Corolla");
-        Car car2 = new Car(attributes2, "Honda", "Civic");
-        Car car3 = new Car(attributes3, "Lexus", "LFA");
-        CarStock carStock = new CarStock();
-        carStock.addCar(car1);
-        carStock.addCar(car2);
-        carStock.addCar(car3);
-
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Double> userAttributes = new ArrayList<>();
-        for(int i = 0; i < 3; i++) {
-            System.out.println("Input value for attribute " + (i+1) + ":");
-            double attributeInput = scanner.nextDouble();
-            userAttributes.addLast(attributeInput);
-        }
-
-        ArrayList<String> preferredMakes = new ArrayList<>();
-        String makeInput = null;
-        while(!Objects.equals(makeInput, "0")){
-            System.out.println("Input preferred make (0 to quit):");
-            makeInput = scanner.next();
-            preferredMakes.add(makeInput);
-            //TODO: make a list of all possible makes in Car class
-        }
-        User user = new User(userAttributes, preferredMakes);
-
-        System.out.println("Best match for this user: " + carStock.findBestMatch(user));
-    }
 }

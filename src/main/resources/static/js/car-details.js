a70f5f9f7d3ba16015165b4e76d90e025ea1d9f2
xMerPeople29 <noah.vanleeuwen@ontariotechu.net>
2026-03-24 09:41:40 -0400

    Added details to car-details page

--- src/main/resources/static/js/car-details.js
+++ src/main/resources/static/js/car-details.js
@@ -10,12 +10,16 @@ fetch("cars.json")
         const car = cars.find((currentCar, index) => String(currentCar.id ?? index) === carId);
 
         if (car) {
-            document.getElementById("carTitle").innerText = `${car.make} ${car.model}`;
-            document.getElementById("carPrice").innerText = `$${car.price.toLocaleString()}`;
+            document.getElementById("carTitle").innerText = `${car.year} ${car.make} ${car.model} - $${car.price.toLocaleString()}`;
             document.getElementById("carBody").innerText = car.bodyType;
-            document.getElementById("carMileage").innerText = `${car.mileage.toLocaleString()} mi`;
+            document.getElementById("carMileage").innerText = `${car.mileage.toLocaleString()} km`;
             document.getElementById("carHp").innerText = car.horsepower;
             document.getElementById("carSeats").innerText = car.seating;
+            document.getElementById("carDrivetrain").innerText = car.drivetrainConfiguration;
+            document.getElementById("carEngine").innerText = `${car.transmission} ${car.cylinders}-cylinder, ${car.engineConfiguration} Configuration`;
+            document.getElementById("carExtColour").innerText = car.extColour;
+            document.getElementById("carIntColour").innerText = car.intColour;
+
         } else {
             document.getElementById("carTitle").innerText = "Car Not Found";
             document.querySelector(".specs-grid").style.display = "none";
